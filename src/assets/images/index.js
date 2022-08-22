77b2b7d4914ada5015986e410b353d714b4a75e8
rere <reyiahornejas+1@gmail.com>
2022-08-22 16:43:11 +0800

    add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,11 @@
 import carousel1 from './carousel_cv1.jpeg';
+import carousel8 from './carousel_cv8.jpeg';
+import carousel9 from './carousel_cv9.jpeg';
+import carousel11 from './carousel_cv11.jpeg';
+import carousel13 from './carousel_cv13.jpeg';
+import carousel15 from './carousel_cv15.jpeg';
+import carousel20 from './carousel_cv20.jpeg';
 import heroImg from './en-hero-img.jpeg';
 
-export { carousel1, heroImg }
+
+export { carousel1, heroImg, carousel8, carousel9, carousel11, carousel13, carousel15, carousel20 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app';
+import "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCdO1Qk_3pHY4xEGDo0ZAg9BxcB5EIsj6g",
-  authDomain: "authentication-f7a68.firebaseapp.com",
-  databaseURL: "https://authentication-f7a68.firebaseio.com",
-  projectId: "authentication-f7a68",
-  storageBucket: "authentication-f7a68.appspot.com",
-  messagingSenderId: "866879786544",
-  appId: "1:866879786544:web:1164e65a4561d6fc0f67f5"
-};
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const auth = app.auth()
+export default app
