8010d106665bc8e2b12b17abe903813019ee0c7b
rere <reyiahornejas+1@gmail.com>
2022-08-23 21:58:29 +0800

    add images for homepage

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,30 @@
-import carousel1 from './carousel_cv1.jpeg';
-import carousel8 from './carousel_cv8.jpeg';
-import carousel9 from './carousel_cv9.jpeg';
-import carousel11 from './carousel_cv11.jpeg';
-import carousel13 from './carousel_cv13.jpeg';
-import carousel15 from './carousel_cv15.jpeg';
-import carousel20 from './carousel_cv20.jpeg';
-import heroImg from './en-hero-img.jpeg';
+import carousel1 from "./carousel_cv1.jpeg";
+import carousel8 from "./carousel_cv8.jpeg";
+import carousel9 from "./carousel_cv9.jpeg";
+import carousel11 from "./carousel_cv11.jpeg";
+import carousel13 from "./carousel_cv13.jpeg";
+import carousel15 from "./carousel_cv15.jpeg";
+import carousel20 from "./carousel_cv20.jpeg";
+import heroImg from "./en-hero-img.jpeg";
 
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import greenCheck from "./checkbox.svg";
 
-export { carousel1, heroImg, carousel8, carousel9, carousel11, carousel13, carousel15, carousel20 }
+export {
+  carousel1,
+  heroImg,
+  carousel8,
+  carousel9,
+  carousel11,
+  carousel13,
+  carousel15,
+  carousel20,
+  facebook,
+  twitter,
+  linkedin,
+  instagram,
+  greenCheck
+};
