62474e4cb068afc5950d60881dbf1e065036c7cc
rere <reyiahornejas+1@gmail.com>
2022-08-31 03:54:55 +0800

    remove unused var

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,12 +1,6 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAIL = 'REGISTER_FAIL';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
-export const LOGOUT = 'LOGOUT';
 export const CREATE_USER = 'CREATE_USER';
-
 
 //RESUME
 export const CREATE_RESUME = "CREATE_RESUME";
